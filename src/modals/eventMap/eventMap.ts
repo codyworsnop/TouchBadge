643ebd622dee4da08479a4062797101421671a28
Cody Worsnop <Cody@Codys-MBP.nv.charter.com>
2018-03-24 21:58:27 -0700

    stopped map panning and zoom, dropped pin

--- src/modals/eventMap/eventMap.ts
+++ src/modals/eventMap/eventMap.ts
@@ -27,9 +27,32 @@ export class eventMap {
       center: latLng, 
       zoom: 15, 
       mapTypeId: google.maps.MapTypeId.ROADMAP,
+      zoomControl: false, 
+      mapTypeControl: false,
+      streetViewControl: false, 
+      fullscreenControl: false, 
+      gestureHandling: 'none',
     };
 
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+   
+    let content = "<h4>Information!</h4>";         
+   
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+   
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+    
+
   }
 
   dismissModal() { 
